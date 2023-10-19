ba6949cd9fd9c8eefe0f0c6c072b17241d8bd547
TEST <test@TESTs-MacBook-Pro.local>
2023-10-19 12:01:51 +0100

    Allowed host url fix

--- BurgerJoint/settings.py
+++ BurgerJoint/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://burger-joint-286ef76e4359.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['burger-joint-286ef76e4359.herokuapp.com', 'localhost']
 
 
 # Application definition
