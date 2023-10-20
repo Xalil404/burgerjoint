ded15762315e1a06d13f85d581413a38f65653c4
TEST <test@TESTs-MacBook-Pro.local>
2023-10-20 15:31:50 +0100

    URL fixes

--- BurgerJoint/urls.py
+++ BurgerJoint/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from Booking_App.views import get_home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_home_page, name='home'),
-    path('accounts', include('allauth.urls')),
+    path('accounts/', include('allauth.urls'), name='account'),
 ]
+
+#Code to serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
