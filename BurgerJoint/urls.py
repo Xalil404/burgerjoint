9412b574f728c9a19be395e7ac9b3e9c451ca6e4
TEST <test@TESTs-MacBook-Pro.local>
2023-10-20 11:46:11 +0100

    home page place holder

--- BurgerJoint/urls.py
+++ BurgerJoint/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Booking_App.views import say_hello
+from Booking_App.views import get_home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', say_hello, name = 'hello'),
+    path('', get_home_page, name = 'get_home_page'),
 ]
