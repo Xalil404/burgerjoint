969802c1cc366ad5c6024292ccf32318df0e3815
TEST <test@TESTs-MacBook-Pro.local>
2023-10-22 17:03:42 +0100

    Favicon links added

---
+++ staticfiles/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".filter").click(function() {
+        var filterValue = $(this).attr("data-filter");
+        if (filterValue === "all") {
+            $(".menu-item").show();
+        } else {
+            $(".menu-item").hide();
+            $("." + filterValue).show();
+        }
+    });
+});
