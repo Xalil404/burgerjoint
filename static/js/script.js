bb2cc3522453583b453f46b699edd49ad268ebf7
TEST <test@TESTs-MacBook-Pro.local>
2023-10-25 19:32:38 +0100

    Added booking deletion confirmation modal

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+//Function to toggle menu on homepage
 $(document).ready(function() {
     $(".filter").click(function() {
         var filterValue = $(this).attr("data-filter");
@@ -10,6 +11,13 @@ $(document).ready(function() {
     });
 });
 
-
-
-
+//Function to delete booking
+$(document).ready(function () {
+    $('#confirmDeleteModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); 
+        var bookingId = button.data('booking-id'); 
+        var deleteButton = $('#confirmDeleteButton');
+        var deleteUrl = button.data('delete-url'); 
+        deleteButton.attr('href', deleteUrl); 
+    });
+});
