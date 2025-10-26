a0aec5de6236513c11bc4ca4ff759763307c5115
Xalil404 <kareemxalil@gmail.com>
2025-10-26 12:19:48 +0000

    Debug to false

--- BurgerJoint/settings.py
+++ BurgerJoint/settings.py
@@ -28,8 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 
 ALLOWED_HOSTS = ['burger-joint-286ef76e4359.herokuapp.com', 'burgerjoint.onrender.com', 'localhost', '127.0.0.1']
